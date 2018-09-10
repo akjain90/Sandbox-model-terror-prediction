3eef110b3b87fb96c96003660d51b9f355ea4a97
Aishvarya Fraunhofer <aishvaryakumarjain@gmail.com>
2018-09-10 17:25:54 +0200

    check

---
+++ ML_predictor/CNN/CNN_training.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+import os
